daed9d7fae681a62b84661fbb3d8abca58a4c44a
Dmitriy <sapa408@gmail.com>
2020-12-10 17:13:18 +0700

    Сделал задачу TodoList Vue

---
+++ TODO List Vue/script.js
@@ -0,0 +1,118 @@
+Vue.component("modal", {
+    template: "#modal-template",
+
+    data: function () {
+        return {
+            onYesFunction: null
+        }
+    },
+
+    methods: {
+        show: function (someFunction) {
+            this.onYesFunction = someFunction;
+            $(this.$refs.confirmDialogTemplate).modal("show");
+        },
+
+        clickYes: function () {
+            this.onYesFunction();
+            $(this.$refs.confirmDialogTemplate).modal("hide");
+        }
+    }
+});
+
+new Vue({
+    el: "#app",
+    data: {
+        items: [],
+        newTodoText: "",
+        newId: 1,
+        hasInputText: true,
+        confirmDialogTitleText: "",
+        confirmDialogBodyText: ""
+    },
+
+    methods: {
+        addNewTodoItem: function () {
+            var text = this.newTodoText;
+
+            if (text.trim().length === 0) {
+                this.hasInputText = false;
+                this.newTodoText = "";
+                return;
+            }
+
+            this.items.push({
+                id: this.newId,
+                text: text,
+                isEditing: false,
+                editText: text
+            });
+
+            this.hasInputText = true;
+            this.newTodoText = "";
+            this.newId++
+        },
+
+        deleteItem: function (item) {
+            this.confirmDialogTitleText = "Удаление задания";
+            this.confirmDialogBodyText = "Вы уверены, что хотите удалить задание?";
+
+            var self = this;
+
+            this.$refs.confirmDialog.show(function () {
+                self.items = self.items.filter(function (x) {
+                    return x !== item;
+                });
+            });
+        },
+
+        clearInputText: function () {
+            if (this.newTodoText !== "") {
+                this.confirmDialogTitleText = "Отмена ввода";
+                this.confirmDialogBodyText = "Вы уверены, что хотите удалить введенный текст?";
+
+                var self = this;
+
+                this.$refs.confirmDialog.show(function () {
+                    self.newTodoText = "";
+                });
+            }
+        },
+
+        editItem: function (item) {
+            item.isEditing = true;
+        },
+
+        saveItemChanges: function (item) {
+            if (item.editText.trim().length === 0) {
+                this.confirmDialogTitleText = "Пустое задание";
+                this.confirmDialogBodyText = "У задания нет текста. Удалить его?";
+
+                var self = this;
+
+                this.$refs.confirmDialog.show(function () {
+                    self.items = self.items.filter(function (i) {
+                        return i !== item;
+                    });
+                });
+
+                return;
+            }
+
+            item.text = item.editText;
+            item.isEditing = false;
+        },
+
+        cancelItemChanges: function (item) {
+            if (item.editText !== item.text) {
+                this.confirmDialogTitleText = "Отмена изменений";
+                this.confirmDialogBodyText = "Вы уверены, что хотите отменить внесенные изменения?";
+
+                this.$refs.confirmDialog.show(function () {
+                    item.editText = item.text;
+                    item.isEditing = false;
+                });
+            }
+        },
+    }
+});
